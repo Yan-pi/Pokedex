b63ac777b861234a0ebf67bad2d22b95fb6580ce
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2023-06-09 08:26:44 -0300

    refactoring vue app

---
+++ pokevue/src/components/btnprev.vue
@@ -0,0 +1,10 @@
+<script></script>
+
+<template>
+  <div class="buttons">
+    <button class="button btn-prev">Prev &lt;</button>
+    <button class="button btn-next">Next &gt;</button>
+  </div>
+</template>
+
+<style></style>
